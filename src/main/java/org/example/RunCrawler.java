29fc46ed63c74cee14449e5e2a44fc1b80b3cf33
aravinds-repo <careerofaravind@gmail.com>
2024-05-27 02:45:36 +0530

    WebCrawler

--- src/main/java/org/example/RunCrawler.java
+++ src/main/java/org/example/RunCrawler.java
@@ -6,13 +6,15 @@ import java.util.List;
 public class RunCrawler {
 
     public static void main(String[] args) throws Exception {
-        List<String> domains = Arrays.asList(
-                "https://www.freepik.com/videos",
-                "https://www.shutterstock.com/"
-        );
-        for (String domain: domains) {
-            MediaCrawlController controller = new MediaCrawlController();
-            controller.startCrawling(domain);
-        }
+        MediaCrawlController controller = new MediaCrawlController();
+
+        // Sample Image Domain
+//        controller.startCrawling("https://www.shutterstock.com/");
+
+        // Sample Audio Domain
+//        controller.startCrawling("https://www.chosic.com/download-audio/27966/");
+
+        // Sample Video Domain
+//        controller.startCrawling("https://www.freepik.com/videos");
     }
 }
